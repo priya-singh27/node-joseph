0cfdb1bcf87d7962093e38fca2da2a5c3f92df4a
priya-singh27 <priya.beingdev@gmail.com>
2025-05-09 21:04:41 +0530

    hello git

--- streams/solve_memory_issue.js
+++ streams/solve_memory_issue.js
@@ -1,7 +1,8 @@
 import fs from 'fs/promises'
+import { openSync } from 'fs';
 
 (async ()=>{
-    
+
     console.time('flag');
 
     const fileHandler = await fs.open('test.txt', 'w');
@@ -13,27 +14,3 @@ import fs from 'fs/promises'
 
     console.timeEnd('flag');
 })();
-
-// (async ()=>{
-//     console.time('flag');
-
-//     const fileHandler = await fs.open('test.txt', 'w');
-
-//     const stream = fileHandler.createWriteStream();
-
-//     console.log(stream.writableHighWaterMark);//size of internal buffer(chunk)
-//     console.log(stream.writableLength);//how much of this buffer is filled 
-
-//     const buffer = Buffer.from('A','ascii')
-//     console.log(buffer)
-//     const notFull = stream.write(buffer);
-//     console.log(notFull);
-//     console.log(stream.writableLength);
-
-//     // for(let i=0; i<1000000; i++){
-//     //     const buff= Buffer.from(` ${i} `,'utf-8');
-//     //     stream.write(buff);
-//     // }
-
-//     console.timeEnd('flag');
-// })();
