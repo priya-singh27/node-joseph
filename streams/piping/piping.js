1556a5fc27ea91cb4c92b44123ece4b250d4a83f
priya-singh27 <priya.beingdev@gmail.com>
2025-05-18 11:37:15 +0530

    piping

--- streams/piping/piping.js
+++ streams/piping/piping.js
@@ -5,6 +5,5 @@ const fs = require('fs/promises');
     const destFile = await fs.open('text-copy.txt', 'w');
     const srcFile = await fs.open('../writable_streams/test.txt');
 
-
     console.timeEnd("piping")
 })
