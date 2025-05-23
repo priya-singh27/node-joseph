cf35949201cec20db0460057b9b16488228cc929
priya-singh27 <priya.beingdev@gmail.com>
2025-05-23 11:19:05 +0530

    chat-app: fix ui, let user ask question on after the other, let all the user in the chatroom see the msg

--- networking/chat-app/client.js
+++ networking/chat-app/client.js
@@ -1,9 +1,59 @@
 const net = require("net");
+const { resolve } = require("path");
+const readline = require("readline/promises");
 
-const client = net.createConnection({host:"127.0.0.1", port: 8080}, ()=>{
-    console.log("Connected to the server");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-client.on("end", ()=>{
+const clearLine = (dir)=>{
+    return new Promise((resolve,reject)=>{
+        process.stdout.clearLine(dir, ()=>{
+            resolve();
+        })
+
+    });
+} 
+
+const moveCursor = (dx, dy)=>{
+    return new Promise((resolve,reject)=>{
+        process.stdout.moveCursor(dx,dy,()=>{
+            resolve();
+        })
+    })
+}
+
+const socket = net.createConnection({host:"127.0.0.1", port: 8080}, async ()=>{
+    console.log("Connected to the server");
+
+    const ask = async()=>{
+        const message = await rl.question("Enter your message> ");
+        
+        //move the cursor one line up
+        await moveCursor(0, -1);
+
+        //clear the current line that the cursor is in 
+        await clearLine(0);
+        const buffer = Buffer.from(message);
+        socket.write(buffer);//send message to the server
+    }
+
+    ask();
+    // socket.write(message);//we are writing string but it will be converted to buffer, we can alos convert to buffer and write. Let's do that
+
+    //get data from the server and then log it
+    socket.on("data",async(data)=>{
+        console.log();
+        await moveCursor(0, -1);
+        await clearLine(0);
+        console.log(data.toString("utf-8"));
+        ask();
+    });
+});
+
+
+socket.on("end", ()=>{
     console.log("Connection ended!")
 });
+
--- networking/chat-app/server.js
+++ networking/chat-app/server.js
@@ -2,8 +2,19 @@ const net = require("net");
 
 const server = net.createServer();
 
+//an array of client sockets
+const clients = []
+
 server.on("connection", (socket)=>{
-    console.log("A new connection received!");
+    socket.on("data", (data)=>{
+        clients.map((socket)=>{
+            socket.write(data);
+        })
+        console.log(data.toString("utf-8"));
+
+        // socket.write(data);//send message to the client
+    });
+    clients.push(socket);
 });
 
 server.listen(8080, '127.0.0.1' ,()=>{
