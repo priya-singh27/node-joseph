2db6a94e21a52e22a0fc7ac7576eebde872e883c
priya-singh27 <priya.beingdev@gmail.com>
2025-05-30 08:05:31 +0530

    udp connection

---
+++ networking/udp/simple-sender.js
@@ -0,0 +1,7 @@
+const dgram = require("dgram");
+const sender = dgram.createSocket("udp4");
+
+sender.send("This is a string", 8000, "127.0.0.1", (err, bytes)=>{
+    if(err) console.log(err);
+    else console.log(bytes)
+});
