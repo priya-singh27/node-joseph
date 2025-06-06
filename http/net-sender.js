810c0ebc6bb6f9985d23432d19193416aeee3881
priya-singh27 <priya.beingdev@gmail.com>
2025-06-06 23:21:12 +0530

    something

--- http/net-sender.js
+++ http/net-sender.js
@@ -2,7 +2,8 @@ const net = require('net');
 
 const socket = net.createConnection({
     host:"localhost",
-    port:"8000"
+    // port:"8000"
+    port:"3000"
 }, ()=>{
     // socket.write("AB");
     const header = Buffer.from('504f5354202f6372656174652d706f737420485454502f312e310d0a436f6e74656e742d547970653a206170706c69636174696f6e2f6a736f6e0d0a6e616d653a2050726979610d0a486f73743a206c6f63616c686f73743a383030300d0a436f6e6e656374696f6e3a206b6565702d616c6976650d0a436f6e74656e742d4c656e6774683a2037380d0a0d0a', 'hex');
@@ -15,7 +16,15 @@ const socket = net.createConnection({
 socket.on('data', (chunk)=> {
     console.log("Received response: ");
     console.log(chunk.toString('utf-8'));
+    console.log('--------------------------')
     console.log(chunk.toString('hex'));
+    console.log('--------------------------')
+
+    chunk.toString('hex').split("0d0a").forEach(ele => {
+        const buffer = Buffer.from(ele, 'hex');
+        let str = buffer.toString('utf-8');
+        console.log(`${ele}: ${str}`);
+    })
 
     socket.end();
 });
--- networking/simple-server.js
+++ networking/simple-server.js
@@ -2,7 +2,7 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
     socket.on('data', (data) =>{
-        console.log(data)
+        console.log(data.toString('utf-8'));
     })
 });
 
