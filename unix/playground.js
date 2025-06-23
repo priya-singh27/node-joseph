683a5f8b36f9b872a43137e203de32c0a276f224
priya-singh27 <priya.beingdev@gmail.com>
2025-06-23 20:58:34 +0530

    stdin, stdout and stderr

---
+++ unix/playground.js
@@ -0,0 +1,27 @@
+const {spawn} = require('node:child_process');
+const {stdin, stdout, stderr} = require('node:process');
+
+//stdin, stdout, stderr
+// stdin.on('data', (data)=>{
+//     // console.log("Got data from stdin: ", data.toString('utf-8'));
+//     stdout.write(`Got data from stdin: , ${data.toString('utf-8')}\n`)
+// });
+// stdout.write("Writing to a writeable stream!");
+// stderr.write("The stderr text!!");
+
+//process id,command argument
+// console.log(process.argv);
+// console.log(process.pid);
+// console.log(process.ppid);
+
+//environment variables
+// console.log(process.env)
+
+//spawning
+const subProcess = spawn("/home/priya/DSA/c++/containers",["first thing first"]);
+
+subProcess.stdout.on("data", (data)=>{
+    console.log(data.toString("utf-8"));
+});
+
+subProcess.stdin.write('Some text that is coming from node!')
